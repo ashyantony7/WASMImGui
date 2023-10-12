cc30c5482c9a770f199faef1060baca3397d05f0
Anthony Benedict <anthony.benedict@celus.io>
2023-10-12 19:40:17 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# WASMImGui
-Template project for ImGui WASM
+# WASM Imgui Template project
+
+This is a template project for building a webassembly application using imgui and emscripten.
+
+## Setup
+
+The following dependencies must be installed
+
+1. CMake, Ninja and Conan:
+   These dependencies are handled using pip in `requirements.txt`. To install them (preferably in a virtual environment) run:
+
+```bash
+pip install -r requirements.txt
+```
+
+2. OpenGL:
+
+We use openGL for rendering. The following dependencies must be installed:
+
+- Mac Os X
+
+```bash
+brew install glfw3
+brew install glew
+```
+3. Emscripten:
+
+Setup a conan profile for emscripten. Save the following file to `~/.conan/profiles/emscripten`:
+
+- Mac Os:
+
+```
+include(default)
+
+[settings]
+os=Emscripten
+arch=wasm
+compiler=clang
+compiler.version=16
+compiler.libcxx=libc++
+build_type=Release
+
+[tool_requires]
+emsdk/3.1.23
+```
+
+Build the application:
+
+```bash
+mkdir build && cd build
+conan install ../conanfile.txt  --build missing --profile:build=default --profile:host=emscripten -s build_type=Release
+source ./conanbuild.sh
+cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --config Release
+```
---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <cstdio>
+#include "bindings/imgui_impl_glfw.h"
+#include "bindings/imgui_impl_opengl3.h"
+#include "imgui.h"
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h>  // Will drag system OpenGL headers
+#include <emscripten.h>
+
+static void glfw_error_callback(int error, const char* description) { fprintf(stderr, "GLFW Error %d: %s\n", error, description); }
+
+int main() {
+  glfwSetErrorCallback(glfw_error_callback);
+  if (!glfwInit()) return 1;
+
+    // Decide GL+GLSL version
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+  // GL ES 2.0 + GLSL 100
+  const char* glsl_version = "#version 100";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+  // GL 3.2 + GLSL 150
+  const char* glsl_version = "#version 150";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+  // GL 3.0 + GLSL 130
+  const char* glsl_version = "#version 130";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+  // Create window with graphics context
+  GLFWwindow* window = glfwCreateWindow(1280, 720, "wasm-imgui", nullptr, nullptr);
+  if (window == nullptr) return 1;
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);  // Enable vsync
+
+  return 0;
+}
